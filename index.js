3667dd24fe9f332eb56e366c780e05e2205938ae
Christopher Kufis <christopherkufis@Christophers-MacBook-Pro.local>
2022-11-18 15:19:59 -0800

    dynamic spa 1.0

---
+++ index.js
@@ -0,0 +1,98 @@
+var inputs = [
+    'Email',
+    'Password',
+]
+var id_format = 'login_input_'
+var id_end_format = ''
+
+document.addEventListener('mouseup',input_listeners,true)
+document.addEventListener('keyup',input_listeners,true)
+document.getElementById('signup_button').addEventListener('click',signup_table)
+
+
+function input_listeners()
+{
+
+    for (var i = 0; i < inputs.length;i++)
+{
+    var tidyinput = inputs[i].replace('_',' ')
+    if (document.activeElement.id === id_format + inputs[i])
+    {
+        if(document.activeElement.value == tidyinput)
+        {
+            document.activeElement.value = '';
+        }
+    }
+    if (document.activeElement.id !== id_format + inputs[i])
+    {
+        if (document.getElementById(id_format + inputs[i]).value === '')
+        {
+            document.getElementById(id_format + inputs[i]).value = tidyinput
+        }
+    }
+}
+}
+function signup_table()
+{
+    var signup_inputs = [
+        'Email',
+        'Phone_Number',
+        'First_Name',
+        'Last_Name',
+        'Password',
+        'Password_confirm',
+    ]
+    if (document.getElementById('signup_table') === null)
+    {
+        var signup_table = document.createElement('table')
+        signup_table.id = 'signup_table'
+        var signup_sign = document.createElement('div')
+        signup_sign.innerHTML = 'Sign Up'
+        signup_table.append(signup_sign)
+        document.getElementById('container_main').append(signup_table)
+        fill_table(signup_table.id,signup_inputs,2)
+        document.removeEventListener('mouseup',input_listeners,true)
+        document.removeEventListener('keyup',input_listeners,true) 
+        inputs = signup_inputs
+        id_format = 'signup_table_input_'
+        document.addEventListener('mouseup',input_listeners,true)
+        document.addEventListener('keyup',input_listeners,true)
+        document.getElementById('login_table_container').remove()
+    }
+    else (alert('error'))
+}
+function fill_table(table_id,input_array,cells_per_row){
+    var rows = input_array.length/cells_per_row
+    var table = document.getElementById(table_id);
+    var row_iterator = 0
+    for (var i = 0;i < input_array.length;i++)
+    {
+        if((i)%cells_per_row === 0)
+        {
+            var new_row = table.insertRow(row_iterator)
+            new_row.id = table_id + '_row_' + row_iterator
+            var new_cell = new_row.insertCell(i%cells_per_row)
+            var cell_insertion = document.createElement('input')
+            cell_insertion.id = table_id + '_input_' + input_array[i]
+            cell_insertion.value = input_array[i].replace('_', ' ')
+            new_cell.append(cell_insertion);
+            row_iterator++
+        }
+        else{
+            var edit_row = document.getElementById(table_id + '_row_' + (row_iterator-1))
+            var new_cell = edit_row.insertCell(i%cells_per_row)
+            var cell_insertion = document.createElement('input')
+            cell_insertion.id = table_id + '_input_' + input_array[i]
+            cell_insertion.value = input_array[i].replace('_', ' ')
+            new_cell.append(cell_insertion)
+        }
+    }
+        var signup_submit_button = document.createElement('button')
+            signup_submit_button.innerHTML = 'submit'
+            signup_submit_button.id = 'signup_submit_button'
+        var signup_form_exit_button = document.createElement('button')
+            signup_form_exit_button.innerHTML = 'exit'
+            signup_form_exit_button.id = 'signup_form_exit_button'
+
+        document.getElementById(table_id).append(signup_form_exit_button,signup_submit_button)
+}
